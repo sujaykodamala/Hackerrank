be0b31d0cddbb26f17cc568cf47a6d48c0144c5d
SujayKodamala <sujaysiddu@gmail.com>
2018-03-10 17:27:16 +0530

    FirstCommit

---
+++ Hackerrank/DataTypes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hackerrank
+{
+    class DataTypes
+    {
+
+        static void Main(string[] args)
+        {
+            int i = 4;
+            double d = 4.0;
+            string s = "Hackerrank";
+            int j = Convert.ToInt32(Console.ReadLine());
+            double e = Convert.ToDouble(Console.ReadLine());
+            string x = Convert.ToString(Console.ReadLine());
+            // Read and save an integer, double, and String to your variables.
+
+            // Print the sum of both integer variables on a new line.
+
+            // Print the sum of the double variables on a new line.
+
+
+            // Concatenate and print the String variables on a new line
+            Console.WriteLine((i + j).ToString());
+            Console.WriteLine((d + e).ToString("F1"));
+            Console.WriteLine(s + x);
+            // The 's' variable above should be printed first.
+
+        }
+    }
+}
---
+++ Hackerrank/Operators.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hackerrank
+{
+    class Operators
+    {
+        static void Main(String[] args)
+        {
+            double meal_cost = Convert.ToDouble(Console.ReadLine());
+            int tip_percent = Convert.ToInt32(Console.ReadLine());
+            int tax_percent = Convert.ToInt32(Console.ReadLine());
+            double tax = Convert.ToDouble(meal_cost * tax_percent / 100);
+            double tip = Convert.ToDouble(meal_cost * tip_percent / 100);
+            int total = Convert.ToInt32(meal_cost + tax + tip);
+            Console.WriteLine("The total meal cost is " + total.ToString() + " dollars.");
+        }
+    }
+}
