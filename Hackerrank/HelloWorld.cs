b49fe00ca5ae4f6718c8bb71a8997404c0592a78
SujayKodamala <sujaysiddu@gmail.com>
2018-03-10 17:08:37 +0530

    Add project files.

---
+++ Hackerrank/HelloWorld.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Hackerrank
+{
+    class Solution
+    {
+        static void Main(String[] args)
+        {
+            String inputString;
+
+            inputString = Console.ReadLine();
+
+            Console.WriteLine("Hello, World.");
+
+        }
+    }
+
+}
